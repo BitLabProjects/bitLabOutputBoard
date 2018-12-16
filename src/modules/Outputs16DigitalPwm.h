f83eac48cf99f9d53d0f54d5a84fd655c5adc9eb
xed89 <simone.bosc@gmail.com>
2018-12-16 22:01:28 +0100

    Cleaned unused code in digital pwm outputs

--- src/modules/Outputs16DigitalPwm.h
+++ src/modules/Outputs16DigitalPwm.h
@@ -13,11 +13,6 @@ public:
 
   void setOutput(uint32_t idxOutput, uint32_t value);
   void onTick();
-  bool getInput50HzIsStable() { return input50HzIsStable; }
-  float getMeasured50HzFrequency()
-  {
-    return ((float)ticksPerSecond) / (lastZeroCrossDurationInTicks * 2.0f);
-  }
 
 private:
   static const uint32_t ticksPerSecond = 10 * 1000;
@@ -25,21 +20,12 @@ private:
   static const uint32_t gateOnTimeTicks = 10;
   static const uint32_t nominalTicksBetweenZeroCrosses = ticksPerSecond / nominalZeroCrossPerSecond;
   static const uint32_t nominalTicksBetweenZeroCrossesMaxDelta = nominalTicksBetweenZeroCrosses / 5;
-  static const bool simulateVAC = true;
 
   static const uint32_t outputsCount = 16;
   DigitalOut outputs[outputsCount];
   uint32_t outputValues[outputsCount];
 
-  InterruptIn main_crossover;
-  //DigitalOut led;
-
-  bool input50HzIsStable;
-  int zeroCrossesCount;
   int ticksSinceZeroCross;
-  int lastZeroCrossDurationInTicks;
-
-  void main_crossover_rise();
 };
 
 #endif
